bc00125111c1537793297afdaddd211c908e3a33
realmortis64 <xmortis64@gmail.com>
2026-02-13 20:38:01 -0500

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1353,26 +1353,6 @@ end)
 
 	
 run(function()
-	local old
-	
-	vape.Categories.Combat:CreateModule({
-		Name = 'NoClickDelay',
-		Function = function(callback)
-			if callback then
-				old = bedwars.SwordController.isClickingTooFast
-				bedwars.SwordController.isClickingTooFast = function(self)
-					self.lastSwing = os.clock()
-					return false
-				end
-			else
-				bedwars.SwordController.isClickingTooFast = old
-			end
-		end,
-		Tooltip = 'Remove the CPS cap'
-	})
-end)
-	
-run(function()
 	local Value
 	
 	Reach = vape.Categories.Combat:CreateModule({
